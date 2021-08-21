a593f06e9b6956bc04964a3b60f132c9f9106fed
dayamedina95 <52513885+dayamedina95@users.noreply.github.com>
2021-08-20 19:55:06 -0500

    Revert "Commit dayana"
    This reverts commit 386981e7bf7518f4ea9f612743eb73df8dfcf591.

--- src/api/commitment.js
+++ src/api/commitment.js
@@ -47,7 +47,7 @@ export function deleteCompApi(id){
             .catch(err => {return err});
 }
 
-//preguntar metodo a Danny
+//preguntar a Danny
 export function getCompByUserApi(data){
     const url = `${urlBackend}control_seguimiento/LISTAR_COMPROMISOS`;
     const params = {
